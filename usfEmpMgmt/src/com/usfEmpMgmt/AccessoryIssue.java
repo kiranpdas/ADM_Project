99fa3ca994858ae1cacf244d317763ee0277b62a
kiranpdas <30925350+kiranpdas@users.noreply.github.com>
2017-12-02 19:20:16 -0500

    summary for adm project
    USF dining

---
+++ usfEmpMgmt/src/com/usfEmpMgmt/AccessoryIssue.java
@@ -0,0 +1,29 @@
+package com.usfEmpMgmt;
+
+import java.util.Date;
+
+public class AccessoryIssue {
+ private String accessoryName;
+ private String accessoryStatus;
+ private Date accessoryIssueDate;
+public String getAccessoryName() {
+	return accessoryName;
+}
+public void setAccessoryName(String accessoryName) {
+	this.accessoryName = accessoryName;
+}
+public String getAccessoryStatus() {
+	return accessoryStatus;
+}
+public void setAccessoryStatus(String accessoryStatus) {
+	this.accessoryStatus = accessoryStatus;
+}
+public Date getAccessoryIssueDate() {
+	return accessoryIssueDate;
+}
+public void setAccessoryIssueDate(Date accessoryIssueDate) {
+	this.accessoryIssueDate = accessoryIssueDate;
+}
+ 
+ 
+}
